8e66941416fbea3c537a7fa65256068f1533bf45
Jacinth <jacinthreuben@gmail.com>
2026-05-28 12:20:48 +0530

    ros2

---
+++ overtaking_car/overtaking_car/jetracer_node.py
@@ -0,0 +1,39 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+# This imports from your existing jetracer library 
+# (We will make sure Python can see it via a symlink)
+from jetracer.nvidia_racecar import NvidiaRacecar
+
+class JetRacerHardwareNode(Node):
+    def __init__(self):
+        super().__init__('jetracer_hardware_node')
+        self.car = NvidiaRacecar()
+        self.subscription = self.create_subscription(
+            Twist,
+            '/cmd_vel',
+            self.cmd_vel_callback,
+            10)
+        self.get_logger().info('JetRacer Hardware Node is active and listening to /cmd_vel')
+
+    def cmd_vel_callback(self, msg):
+        # Translate ROS 2 Twist messages to JetRacer steering/throttle
+        self.car.throttle = msg.linear.x
+        self.car.steering = msg.angular.z
+        self.get_logger().info(f'Throttle: {msg.linear.x:.2f}, Steering: {msg.angular.z:.2f}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = JetRacerHardwareNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.car.throttle = 0.0
+        node.car.steering = 0.0
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ overtaking_car/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'overtaking_car'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jetson',
+    maintainer_email='jetson@todo.todo',
+    description='ROS 2 Overtaking Package for JetRacer',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'jetracer_node = overtaking_car.jetracer_node:main',
+        ],
+    },
+)
