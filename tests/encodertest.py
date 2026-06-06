049e0c751184d1394e06b9363b54c4c73eadc8f5
Jacinth <jacinthreuben@gmail.com>
2026-06-06 23:45:42 +0530

    test

--- tests/encodertest.py
+++ tests/encodertest.py
@@ -1,15 +1,4 @@
 import serial
-import struct
-
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout=2)
-print("Packet structure analysis...\n")
-
-packets = []
-while len(packets) < 20:
-    if ser.read(1) == b'\x55':
-        data = ser.read(30)
-        if len(data) == 30:
-import serial
 
 ser = serial.Serial('/dev/ttyACM1', 115200, timeout=2)
 print("Raw bytes from ttyACM1 (hex):")
